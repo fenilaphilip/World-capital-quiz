d4b9aa536c80971e215ca9358a3cfe499acd8e28
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2024-02-13 16:36:19 +0100

    database added

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+    user:"postgres",
+    host:"localhost",
+    database:"World",
+    password: process.env.db_pass,
+    port: 5432,   
+});
+
+db.connect();
+
+let quiz = []
+db.query("SELECT * FROM capitals", (err,res)=>{
+    if(err){
+        console.error("Error executing query", err.stack0);
+    }else{
+        quiz = res.rows;
+    }
+    db.end();
+})
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
+let totalCorrect = 0;
+
 app.get("/", (req,res)=>{
-res.render("index.ejs");
+res.render("index.ejs", {
+    totalScore: totalCorrect
+});
 });
 
 app.post("/submit", (req,res)=>{
-    console.log ("hello")
+    console.log ("hello");
+    res.render("index.ejs");
 })
 
 app.listen(port,()=>{
