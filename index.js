fe9fbe65f8d1a2b986aa8ac836ff9e1254e7b373
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2024-02-14 11:39:22 +0100

    document formated

--- index.js
+++ index.js
@@ -6,41 +6,41 @@ const app = express();
 const port = 3000;
 
 const db = new pg.Client({
-    user:"postgres",
-    host:"localhost",
-    database:"World",
+    user: "postgres",
+    host: "localhost",
+    database: "World",
     password: process.env.db_pass,
-    port: 5432,   
+    port: 5432,
 });
 
 db.connect();
 
 let quiz = []
-db.query("SELECT * FROM capitals", (err,res)=>{
-    if(err){
+db.query("SELECT * FROM capitals", (err, res) => {
+    if (err) {
         console.error("Error executing query", err.stack0);
-    }else{
+    } else {
         quiz = res.rows;
     }
     db.end();
 })
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let totalCorrect = 0;
 
-app.get("/", (req,res)=>{
-res.render("index.ejs", {
-    totalScore: totalCorrect
-});
+app.get("/", (req, res) => {
+    res.render("index.ejs", {
+        totalScore: totalCorrect
+    });
 });
 
-app.post("/submit", (req,res)=>{
-    console.log ("hello");
+app.post("/submit", (req, res) => {
+    console.log("hello");
     res.render("index.ejs");
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server is sucessfully running on port ${port}. Go to https://localhost:${port}`)
 })
